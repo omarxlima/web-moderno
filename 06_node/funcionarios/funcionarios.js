48115f53f0c73d775ed02c8cf1c6e7ecf8dbc056
omarxlima <marxmaciel@gmail.com>
2025-01-04 16:03:25 -0300

    desasfio filtro

--- 06_node/funcionarios/funcionarios.js
+++ 06_node/funcionarios/funcionarios.js
@@ -4,7 +4,24 @@ const url = 'http://files.cod3r.com.br/curso-js/funcionarios.json'
 //client http
 const axios = require('axios')
 
+const chineses = f => f.pais === 'China'
+const mulheres = f => f.genero === 'F'
+const menorSalario  = (func, funcAtual) => {
+    return func.salario < funcAtual ? func : funcAtual
+}
+
 axios.get(url).then(response => {
     const funcionarios = response.data
-    console.log(funcionarios)
+    // console.log(funcionarios)
+
+
+    //mullher chinesa com menor salario
+
+    const func = funcionarios
+                .filter(chineses)
+                .filter(mulheres)
+                .reduce(menorSalario)
+
+                console.log(func)
+
 })
