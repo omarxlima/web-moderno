7df25ba54e1ba427864d85f066c78c5ae3ad1173
Marx Lima <marxmaciel@gmail.com>
2025-01-10 11:49:36 -0300

    middlewares

---
+++ 06_node/16_midlewares.js
@@ -0,0 +1,28 @@
+// midleware pattern (chain of responsability)
+
+const passo1 = (ctx, next) => {
+    ctx.valor1 = 'mid1'
+    next()
+}
+
+const passo2 = (ctx, next) => {
+    ctx.valor2 = 'mid2'
+    next()
+}
+
+const passo3 = (ctx, next) => ctx.valor3 = 'mid3'
+
+const exec = (ctx, ...middlewares) => {
+    const execPasso = indice => {
+        middlewares && indice < middlewares.length &&
+        middlewares[indice](ctx, () => execPasso(indice + 1)) 
+    }
+    execPasso(0)
+}
+
+const ctx = {}
+exec(ctx, passo1, passo2, passo3)
+
+console.log(ctx)
+
+//no framework Express midleware tem 3 parametros a requisicao, resposta e o next
